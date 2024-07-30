04dba6840597e19d5eee5a6b4d704340117a4ad9
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-30 18:38:07 +0300

    fix: Add escape

--- components/Header.jsx
+++ components/Header.jsx
@@ -29,15 +29,15 @@ export default function Header() {
                 </li>
             </ul>
             <div className="description">
-                <p>
-                    Merhaba, ben Barış Kahraman. 1999 yılında İstanbul'da doğdum ve şu an Bahçeşehir ilçesinde ikamet ediyorum. Yıldız Teknik Üniversitesi'nde Kontrol ve Otomasyon Mühendisliği Lisans bölümünde eğitim alıyorum. Ayrıca, Marmara Üniversitesi'nde Bilgisayar Programcılığı ön lisans eğitimi alıyorum.
-                </p>
-                <p>
-                    Acunmedya Akademi'den almakta olduğum Genişletilmiş Yazılım Uzmanlığı - Frontend eğitimi, bilgi ve becerilerimi daha da geliştirmeme yardımcı oluyor. Bu süreçte, kendimi sürekli olarak yenilemeyi ve geliştirmeyi amaçlıyorum. Ayrıca, bilgisayar programcılığı alanındaki eğitimim de devam ediyor, bu da bana geniş bir perspektif kazandırıyor.
-                </p>
-                <p>
-                    Amacım, yazılım dünyasında iyi bir takım arkadaşı olmanın yanı sıra özellikle Frontend ve ardından Backend alanında etkili bir geliştirici olmaktır. Yaptığım her adımda öğrenmeye ve kendimi daha da ileriye taşımaya odaklanıyorum.
-                </p>
+            <p>
+              Merhaba, ben Barış Kahraman. 1999 yılında İstanbul&#39;da doğdum ve şu an Bahçeşehir ilçesinde ikamet ediyorum. Yıldız Teknik Üniversitesi&#39;nde Kontrol ve Otomasyon Mühendisliği Lisans bölümünde eğitim alıyorum. Ayrıca, Marmara Üniversitesi&#39;nde Bilgisayar Programcılığı ön lisans eğitimi alıyorum.
+            </p>
+            <p>
+              Acunmedya Akademi&#39;den almakta olduğum Genişletilmiş Yazılım Uzmanlığı - Frontend eğitimi, bilgi ve becerilerimi daha da geliştirmeme yardımcı oluyor. Bu süreçte, kendimi sürekli olarak yenilemeyi ve geliştirmeyi amaçlıyorum. Ayrıca, bilgisayar programcılığı alanındaki eğitimim de devam ediyor, bu da bana geniş bir perspektif kazandırıyor.
+            </p>
+            <p>
+              Amacım, yazılım dünyasında iyi bir takım arkadaşı olmanın yanı sıra özellikle Frontend ve ardından Backend alanında etkili bir geliştirici olmaktır. Yaptığım her adımda öğrenmeye ve kendimi daha da ileriye taşımaya odaklanıyorum.
+            </p>
             </div>
         </header>
     </section>
