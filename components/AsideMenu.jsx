8fb3dcf34ded5cf2c52b144dbba4e1e3d38dd2bc
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-30 16:47:42 +0300

    feat: Add aside menu

---
+++ components/AsideMenu.jsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile, faUserLarge, faLinesLeaning, faCode } from '@fortawesome/free-solid-svg-icons';
+
+export default function AsideMenu() {
+  return (
+    <aside>
+      <nav className="navigation">
+        <ul>
+          <li>
+            <a href="../public/files/barisKahramanResume.pdf" target="_blank" className="tooltip" data-tooltip="CV Görüntüle">
+              <FontAwesomeIcon icon={faFile} />
+            </a>
+          </li>
+          <li>
+            <a className="tooltip" href="#header" data-tooltip="Hakkımda">
+              <FontAwesomeIcon icon={faUserLarge} />
+            </a>
+          </li>
+          <li>
+            <a className="tooltip" href="#tools" data-tooltip="Öğrendiklerim">
+              <FontAwesomeIcon icon={faLinesLeaning} />
+            </a>
+          </li>
+          <li>
+            <a className="tooltip" href="#projects" data-tooltip="Projelerim">
+              <FontAwesomeIcon icon={faCode} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  )
+}
