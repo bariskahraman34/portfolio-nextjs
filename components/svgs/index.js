f5d6aa3f3c52a09e0c0f75645cbc2a16934be6e3
Barış Kahraman <baris-kahra@hotmail.com>
2024-08-01 00:58:31 +0300

    feat: Add new svgs and export svgs

---
+++ components/svgs/index.js
@@ -0,0 +1,3 @@
+export { default as NextjsIcon } from "./nextjs-svgrepo-com.svg"
+export { default as MaterialIcon } from "./material-ui.svg"
+export { default as TypescriptIcon } from "./typescript.svg"
