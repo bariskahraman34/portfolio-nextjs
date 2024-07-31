559370864d46bf40464b4d6dfb99212ddef247c4
Barış Kahraman <baris-kahra@hotmail.com>
2024-08-01 00:56:34 +0300

    chore: add webpack for using custom svgs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
   images: {
     domains: ['htidrheaexbcibvimmjg.supabase.co']
-  }
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: {icon: true} }]
+    })
+
+    return config
+  },
+
 };
 
 export default nextConfig;
